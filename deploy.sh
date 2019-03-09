9ac88eddd6b4ff345f5307342e73b3cd5eaa9255
Ryan Ray <ryan314r@gmail.com>
2019-03-09 17:25:18 -0600

    Publish

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 openssl aes-256-cbc -K $encrypted_ee51214d0271_key -iv $encrypted_ee51214d0271_iv -in secrets.tar.enc -out secrets.tar -d
 tar xf secrets.tar
 chmod +x gradlew
-./gradlew :app:buildRelease
+./gradlew publish
