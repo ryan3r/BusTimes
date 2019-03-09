833f8f2896976d1a69b156b6728d4c8aaf696f47
Ryan Ray <ryan314r@gmail.com>
2019-03-09 17:09:36 -0600

    Add secrets

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-openssl aes-256-cbc -K $encrypted_ee51214d0271_key -iv $encrypted_ee51214d0271_iv -in android.jks.enc -out android.jks -d
-rvm install 2.3.1
-gem install supply
+openssl aes-256-cbc -K $encrypted_ee51214d0271_key -iv $encrypted_ee51214d0271_iv -in secrets.tar.enc -out secrets.tar -d
+tar xf secrets.tar
 chmod +x gradlew
 ./gradlew :app:buildRelease
