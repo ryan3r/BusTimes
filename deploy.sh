bfa7fd8258663d66eb32f0e5e6f21ff38f5b8254
Ryan Ray <ryan314r@gmail.com>
2019-03-09 21:32:44 -0600

    Switch keystores

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 openssl aes-256-cbc -K $encrypted_ee51214d0271_key -iv $encrypted_ee51214d0271_iv -in secrets.tar.enc -out secrets.tar -d
 tar xf secrets.tar
-sed "s/---MY-PASSWORD-HERE---/$KEY_PASSWORD/" -i app/build.gradle
-echo $MY_PASSWORD
-cat app/build.gradle
-#chmod +x gradlew
-#./gradlew :app:publish
+sed "s/---MY-PASSWORD-HERE---/$KEY_PASSWORD/g" -i app/build.gradle
+chmod +x gradlew
+./gradlew :app:publish
