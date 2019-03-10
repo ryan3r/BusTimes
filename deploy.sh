141a2381157aaae0e1b446fd13a6fe25867444de
Ryan Ray <ryan314r@gmail.com>
2019-03-09 21:57:24 -0600

    Fix sed command

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 openssl aes-256-cbc -K $encrypted_ee51214d0271_key -iv $encrypted_ee51214d0271_iv -in secrets.tar.enc -out secrets.tar -d
 tar xf secrets.tar
-sed "s/---MY-PASSWORD-HERE---/$KEY_PASSWORD/g" -i app/build.gradle
+sed "s/---MY-PASSWORD-HERE---/$KEY_PASSWORD/" -i app/build.gradle
 cat app/build.gradle
 #chmod +x gradlew
 #./gradlew :app:publish
